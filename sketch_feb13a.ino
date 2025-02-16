71e7971fadb099273719ce4a8170153e989ce175
-Kurby- <137503165+YourKurby@users.noreply.github.com>
2025-02-16 21:35:14 +0100

    Add files via upload

--- sketch_feb13a.ino
+++ sketch_feb13a.ino
@@ -2,7 +2,6 @@
 #include <Keypad.h>
 #define PIN 11
 #define NUMPIXEL 12
-#define Taster 
 
 const byte COLS = 4; //4 Spalten const byte ROWS = 4; //4 Zeilen //Die Ziffern und Zeichen des Keypads werden eingegeben:
 const byte ROWS = 4;
@@ -39,10 +38,7 @@ void loop()
 {
   pixels.clear();
   pixels.show();
-  Taste = Tastenfeld.getKey();
 
-  while(hit == true) // Leuft bis fehler gemacht wurde
-  {
     int anzahlT;
     for(lvl < 16; lvl++;)
     {
@@ -67,94 +63,103 @@ void loop()
        ms = ms - 10;
       }
 
-      for(anzahlB != 0; anzahlB--;)
+      for(anzahlB > 0; anzahlB--;)
       {
-        randNumber = random(16);
-        pixels.setPixelColor(randNumber, pixels.Color(0, 0, 255));
-        pixels.show();
-        delay(ms);
-        pixels.clear();
-        pixels.show();
 
-          switch (randNumber) {
-          case 0:
-              H[anzahlB] = 'D';
-            break;  
-          case 1:
-              H[anzahlB] = '#';
-            break;
-          case 2:
-              H[anzahlB] = '0';
-            break;
-          case 3:
-              H[anzahlB] = '*';
-            break;
-          case 4:
-              H[anzahlB] = '7';
-            break;
-          case 5:
-              H[anzahlB] = '8';
-            break;
-          case 6:
-              H[anzahlB] = '9';
-            break;
-          case 7:
-              H[anzahlB] = 'C';
-            break;
-          case 8:
-              H[anzahlB] = 'B';
-            break;
-          case 9:
-              H[anzahlB] = '6';
-            break;
-          case 10:
-              H[anzahlB] = '5';
-            break;
-          case 11:
-              H[anzahlB] = '4';
-            break;
-          case 12:
-              H[anzahlB] = '1';
-            break;
-          case 13:
-              H[anzahlB] = '2';
-            break;
-          case 14:
-              H[anzahlB] = '3';
-            break;
-          case 15:
-              H[anzahlB] = 'A';
-            break;
-          default:
-            break;
-        }
-
-      }
-
-      for(anzahlT>0; anzahlT--;)
-      {
-        if(Taste == H[anzahlT])
+        if(hit = true)
         {
-          pixels.setPixelColor(1, pixels.Color(0, 255, 0));
-          pixels.show();
-          delay(200);
-          pixels.clear();
-          pixels.show();
-        }else if(Taste != H[anzahlT])
-        {
-          pixels.setPixelColor(1, pixels.Color(255, 0, 0));
-          pixels.show();
-          delay(200);
-          pixels.clear();
-          pixels.show();
-          hit = false;
           break;
+        }else
+        {
+
+         randNumber = random(16);
+         pixels.setPixelColor(randNumber, pixels.Color(0, 0, 255));
+         pixels.show();
+         delay(ms);
+         pixels.clear();
+          pixels.show();
+
+           switch (randNumber) {
+           case 0:
+               H[anzahlB] = 'D';
+             break;  
+           case 1:
+               H[anzahlB] = '#';
+             break;
+           case 2:
+               H[anzahlB] = '0';
+             break;
+           case 3:
+               H[anzahlB] = '*';
+             break;
+           case 4:
+               H[anzahlB] = '7';
+             break;
+           case 5:
+               H[anzahlB] = '8';
+             break;
+           case 6:
+               H[anzahlB] = '9';
+             break;
+           case 7:
+               H[anzahlB] = 'C';
+             break;
+           case 8:
+               H[anzahlB] = 'B';
+             break;
+           case 9:
+               H[anzahlB] = '6';
+             break;
+           case 10:
+               H[anzahlB] = '5';
+             break;
+           case 11:
+               H[anzahlB] = '4';
+             break;
+           case 12:
+               H[anzahlB] = '1';
+             break;
+           case 13:
+               H[anzahlB] = '2';
+             break;
+           case 14:
+               H[anzahlB] = '3';
+             break;
+           case 15:
+               H[anzahlB] = 'A';
+             break;
+           default:
+             break;
+         }
+
+      
+
+          for(anzahlT > 0; anzahlT--;)
+         {
+          Taste = Tastenfeld.getKey();
+          if(Taste == H[anzahlT])
+          {
+            pixels.setPixelColor(0, pixels.Color(0, 255, 0));
+            pixels.show();
+            delay(200);
+            pixels.clear();
+            pixels.show();
+          }else if(Taste != H[anzahlT])
+          {
+            Serial.print("*");
+            pixels.setPixelColor(0, pixels.Color(255, 0, 0));
+            pixels.show();
+            delay(200);
+            pixels.clear();
+            pixels.show();
+            hit = false;
+             break;
+           }
+
         }
 
       }
 
     }
-
-  }
 
 }
